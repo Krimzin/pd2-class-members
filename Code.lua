b969e224a6f4701128069bf4a98c114f7f5e8c0d
Krimzin <email.krimzin@gmail.com>
2022-07-18 14:27:06 +1200

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -3,9 +3,9 @@ KrimzinCore.require(KrimzinCore.MOD_PATH .. "lua/core/Debug.lua")
 local function write_engine_class_members(path_format)
 	local classes = debug.getregistry()[5]
 
-	for k, v in pairs(classes) do
-		local file = io.open(path_format:format(v.type_name), "w+")
-		file:write(KrimzinCore.Debug.format_member_names(v))
+	for name, class in pairs(classes) do
+		local file = io.open(path_format:format(name), "w+")
+		file:write(KrimzinCore.Debug.format_member_names(class))
 		file:close()
 	end
 end
